34c014df20f4cbfd0217961dd9b5962cbade17ec
Michael Sundqvist <michael.sundqvist1@gmail.com>
2020-02-28 17:02:38 +0200

    Code formatting

--- src/ifcconvert/JsonSerializer.cpp
+++ src/ifcconvert/JsonSerializer.cpp
@@ -53,29 +53,30 @@ namespace {
 namespace {
     std::map<std::string, std::string> argument_name_map;
 
-    // Format an IFC attribute and maybe returns as string. Only literal scalar 
-    // values are converted. Things like entity instances and lists are omitted.
+    // Format an IFC attribute and maybe returns as string. Only literal scalar values are converted.
+    // Things like entity instances and lists are omitted.
     boost::optional<std::string> format_attribute(const Argument* argument, IfcUtil::ArgumentType argument_type, const std::string& argument_name) {
         boost::optional<std::string> value;
 
-        // Hard-code lat-lon as it represents an array
-        // of integers best emitted as a single decimal
-        if (argument_name == "IfcSite.RefLatitude" ||
-            argument_name == "IfcSite.RefLongitude")
-        {
+        // Hard-code lat-lon as it represents an array of integers best emitted as a single decimal
+        if (argument_name == "IfcSite.RefLatitude" || argument_name == "IfcSite.RefLongitude") {
             std::vector<int> angle = *argument;
             double deg;
+
             if (angle.size() >= 3) {
                 deg = angle[0] + angle[1] / 60. + angle[2] / 3600.;
-                int prec = 8;
+                int precision = 8;
+
                 if (angle.size() == 4) {
                     deg += angle[3] / (1000000. * 3600.);
-                    prec = 14;
+                    precision = 14;
                 }
+
                 std::stringstream stream;
-                stream << std::setprecision(prec) << deg;
+                stream << std::setprecision(precision) << deg;
                 value = stream.str();
             }
+
             return value;
         }
 
@@ -83,97 +84,118 @@ namespace {
             case IfcUtil::Argument_BOOL: {
                 const bool b = *argument;
                 value = b ? "true" : "false";
-                break; }
+                break;
+            }
             case IfcUtil::Argument_DOUBLE: {
                 const double d = *argument;
                 std::stringstream stream;
                 stream << d;
                 value = stream.str();
-                break; }
+                break;
+            }
             case IfcUtil::Argument_STRING:
             case IfcUtil::Argument_ENUMERATION: {
                 value = static_cast<std::string>(*argument);
-                break; }
+                break;
+            }
             case IfcUtil::Argument_INT: {
                 const int v = *argument;
                 std::stringstream stream;
                 stream << v;
                 value = stream.str();
-                break; }
+                break;
+            }
             case IfcUtil::Argument_ENTITY_INSTANCE: {
                 IfcUtil::IfcBaseClass* e = *argument;
+
                 if (Type::IsSimple(e->type())) {
                     IfcUtil::IfcBaseType* f = (IfcUtil::IfcBaseType*) e;
                     value = format_attribute(f->getArgument(0), f->getArgumentType(0), argument_name);
-                } else if (e->is(IfcSchema::Type::IfcSIUnit) || e->is(IfcSchema::Type::IfcConversionBasedUnit)) {
+                }
+                else if (e->is(IfcSchema::Type::IfcSIUnit) || e->is(IfcSchema::Type::IfcConversionBasedUnit)) {
                     // Some string concatenation to have a unit name as a XML attribute.
-
                     std::string unit_name;
 
                     if (e->is(IfcSchema::Type::IfcSIUnit)) {
                         IfcSchema::IfcSIUnit* unit = (IfcSchema::IfcSIUnit*) e;
                         unit_name = IfcSchema::IfcSIUnitName::ToString(unit->Name());
+                        
                         if (unit->hasPrefix()) {
                             unit_name = IfcSchema::IfcSIPrefix::ToString(unit->Prefix()) + unit_name;
                         }
-                    } else {
+                    }
+                    else {
                         IfcSchema::IfcConversionBasedUnit* unit = (IfcSchema::IfcConversionBasedUnit*) e;
                         unit_name = unit->Name();
                     }
 
                     value = unit_name;
-                } else if (e->is(IfcSchema::Type::IfcLocalPlacement)) {
+                }
+                else if (e->is(IfcSchema::Type::IfcLocalPlacement)) {
                     IfcSchema::IfcLocalPlacement* placement = e->as<IfcSchema::IfcLocalPlacement>();
-                    gp_Trsf trsf;
+                    gp_Trsf transform;
                     IfcGeom::Kernel kernel;
 
-                    if (kernel.convert(placement, trsf)) {
+                    if (kernel.convert(placement, transform)) {
                         std::stringstream stream;
+
                         for (int i = 1; i < 5; ++i) {
                             for (int j = 1; j < 4; ++j) {
-                                const double trsf_value = trsf.Value(j, i);
-                                stream << trsf_value << " ";
+                                const double transform_value = transform.Value(j, i);
+                                stream << transform_value << " ";
                             }
+
                             stream << ((i == 4) ? "1" : "0 ");
                         }
                         value = stream.str();
                     }				
                 }
-                break; }
+                break;
+            }
             default:
                 break;
         }
+
         return value;
     }
 
     // Appends to a node with possibly existing attributes
     json::reference& format_entity_instance(IfcUtil::IfcBaseEntity* instance, json::reference& child, json::reference& tree, bool as_link = false) {
         const unsigned n = instance->getArgumentCount();
+
         for (unsigned i = 0; i < n; ++i) {
             try {
                 instance->getArgument(i);
-            } catch (const std::exception&) {
+            }
+            catch (const std::exception&) {
                 Logger::Error("Expected " + std::to_string(n) + " attributes for:", instance->entity);
                 break;
             }
+
             const Argument* argument = instance->getArgument(i);
+
             if (argument->isNull()) continue;
 
             std::string argument_name = instance->getArgumentName(i);
             std::map<std::string, std::string>::const_iterator argument_name_it;
             argument_name_it = argument_name_map.find(argument_name);
+
             if (argument_name_it != argument_name_map.end()) {
                 argument_name = argument_name_it->second;
             }
+
             const IfcUtil::ArgumentType argument_type = instance->getArgumentType(i);
 
             const std::string qualified_name = IfcSchema::Type::ToString(instance->type()) + "." + argument_name;
             boost::optional<std::string> value;
+
             try {
                 value = format_attribute(argument, argument_type, qualified_name);
-            } catch (const std::exception& e) {
+            }
+            catch (const std::exception& e) {
                 Logger::Error(e);
-            } catch (const Standard_ConstructionError& e) {
+            }
+            catch (const Standard_ConstructionError& e) {
                 Logger::Error(e.GetMessageString(), instance->entity);
             }
 
@@ -182,7 +204,8 @@ namespace {
                     if (argument_name == "id") {
                         child["<xmlattr>.xlink:href"] = std::string("#") + *value;
                     }
-                } else {
+                }
+                else {
                     std::stringstream stream;
                     stream << "<xmlattr>." << argument_name;
                     child[stream.str()] = *value;
@@ -203,7 +226,7 @@ namespace {
     }
 
     std::string qualify_unrooted_instance(IfcUtil::IfcBaseClass* inst) {
-        return IfcSchema::Type::ToString(inst->type()) + "_" + boost::lexical_cast<std::string>(inst->entity->id());
+        return IfcSchema::Type::ToString(inst->type()) + "_" + std::to_string(inst->entity->id());
     }
 
     // A function to be called recursively. Template specialization is used 
@@ -212,21 +235,24 @@ namespace {
     json::reference& descend(A* instance, json::reference& ref) {
         if (instance->is(IfcSchema::Type::IfcObjectDefinition)) {
             return descend(instance->template as<IfcSchema::IfcObjectDefinition>(), ref);
-        } else {
+        }
+        else {
             return format_entity_instance(instance, ref);
         }
     }
 
-    // Returns related entity instances using IFC's objectified relationship
-    // model. The second and third argument require a member function pointer.
+    // Returns related entity instances using IFC's objectified relationship model.
+    // The second and third argument require a member function pointer.
     template <typename T, typename U, typename V, typename F, typename G>
     typename V::list::ptr get_related(T* t, F f, G g) {
         typename U::list::ptr li = (*t.*f)()->template as<U>();
         typename V::list::ptr acc(new typename V::list);
+
         for (typename U::list::it it = li->begin(); it != li->end(); ++it) {
             U* u = *it;
             acc->push((*u.*g)()->template as<V>());
         }
+
         return acc;
     }
 
@@ -282,9 +308,11 @@ namespace {
 
             for (IfcPropertySetDefinition::list::it it = property_sets->begin(); it != property_sets->end(); ++it) {
                 IfcPropertySetDefinition* pset = *it;
+
                 if (pset->is(Type::IfcPropertySet)) {
                     format_entity_instance(pset, child, true);
                 }
+
                 if (pset->is(Type::IfcElementQuantity)) {
                     format_entity_instance(pset, child, true);
                 }
@@ -308,6 +336,7 @@ namespace {
 
         if (product->is(Type::IfcProduct)) {
             std::map<std::string, IfcPresentationLayerAssignment*> layers = IfcGeom::Kernel::get_layers(product->as<IfcProduct>());
+
             for (std::map<std::string, IfcPresentationLayerAssignment*>::const_iterator it = layers.begin(); it != layers.end(); ++it) {
                 // IfcPresentationLayerAssignments don't have GUIDs (only optional Identifier) so use name as the ID.
                 // Note that the IfcPresentationLayerAssignment passed here doesn't really matter as as_link is true
@@ -318,6 +347,7 @@ namespace {
             }
 
             IfcRelAssociates::list::ptr associations = product->HasAssociations();
+
             for (IfcRelAssociates::list::it it = associations->begin(); it != associations->end(); ++it) {
                 if ((*it)->as<IfcRelAssociatesMaterial>()) {
                     IfcMaterialSelect* mat = (*it)->as<IfcRelAssociatesMaterial>()->RelatingMaterial();
@@ -335,10 +365,12 @@ namespace {
     void format_properties(IfcProperty::list::ptr properties, json::reference& node) {
         for (IfcProperty::list::it it = properties->begin(); it != properties->end(); ++it) {
             IfcProperty* p = *it;
+
             if (p->is(Type::IfcComplexProperty)) {
                 IfcComplexProperty* complex = (IfcComplexProperty*) p;
                 format_properties(complex->HasProperties(), node);
-            } else {
+            }
+            else {
                 format_entity_instance(p, node);
             }
         }
@@ -349,6 +381,7 @@ namespace {
         for (IfcPhysicalQuantity::list::it it = quantities->begin(); it != quantities->end(); ++it) {
             IfcPhysicalQuantity* p = *it;
             json::reference& node2 = format_entity_instance(p, node);
+
             if (p->is(Type::IfcPhysicalComplexQuantity)) {
                 IfcPhysicalComplexQuantity* complex = (IfcPhysicalComplexQuantity*)p;
                 format_quantities(complex->HasQuantities(), node2);
